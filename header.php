a03d317887b1ddc60dcb4babad99e14a7980867f
Maximilian Roll <mroll.mmt-b2022@fh-salzburg.ac.at>
2023-04-23 19:47:31 +0200

    Added initial files and header

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Sound Meteor - <?= $pagetitle ?></title>
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="logo"><a href="index.php"><img src="./media/SoundMeteor.svg" alt="Logo"></a></div>
+            <ul>
+                <li><a href="music.php">Music</a></li>
+                <li><a href="comets.php">Comets</a></li>
+                <li>Peferences</li>
+                <li>About</li>
+            </ul>
+        </nav>
+        <div class="login">
+
+        </div>
+    </header>
+    <main>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include "functions.php";
+$pagetitle = "Home";
+
+include "header.php";
+?>
+
+<?php
+include "footer.php";
+?>
