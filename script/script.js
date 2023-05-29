31f236f3a35c0ba64ed9741cd399a332585b5cb2
Maximilian Roll <mroll.mmt-b2022@fh-salzburg.ac.at>
2023-05-29 23:01:14 +0200

    Merged changes

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
 const auth_token = "Discogs token=tmaswzbNQlPUxhekudJyHsNNbUZxMXaPtxXfUYXa";
 
 const form = document.getElementById("form");
-let isMenuOpen, isUserOpen = false;
+let isMenuOpen,
+  isUserOpen = false;
 
 if (form != null) {
   form.addEventListener(
@@ -66,24 +67,37 @@ let toggleMenuOpen = () => {
   document.getElementById("menu-icon").classList.toggle("menu-toggle-closed");
 };
 
+
 let toggleUserOpen = () => {
-    isUserOpen = !isUserOpen;
-    document
-      .getElementsByClassName("usr-overlay")[0]
-        .classList.toggle("usr-open");
-    document
-        .getElementsByClassName("usr-backdrop")[0]
-        .classList.toggle("usr-backdrop-enabled");
-  };
+  const userListenener = (new AbortController()).signal;
+  isUserOpen = !isUserOpen;
+  document
+    .getElementsByClassName("usr-overlay")[0]
+    .classList.toggle("usr-open");
+  document
+    .getElementsByClassName("usr-backdrop")[0]
+    .classList.toggle("usr-backdrop-enabled");
+    document.body.addEventListener("keydown", (e) => {
+        if (!isUserOpen) {
+            userListenener.abort;
+        } else {
+            e = e || window;
+            if (e.key === "Escape") {
+                toggleUserOpen();
+            }
+        }
+    }, {signal: userListenener});
+  }
 
 window.onresize = () => {
   if (window.screenX < 1000 && isMenuOpen) {
-    toggleIsOpen();
+    toggleMenuOpen();
   }
 };
 
 async function loadNewAlbums() {
-  const year = Math.floor(Math.random() * (new Date().getFullYear() - 2005)) + 2005;
+  const year =
+    Math.floor(Math.random() * (new Date().getFullYear() - 2005)) + 2005;
   let query = `https://api.discogs.com/database/search?style=drum+n+bass&per_page=10&format=album&type=release&type=master&year=${year}`;
   try {
     const res_pages = await fetch(query, {
@@ -91,13 +105,16 @@ async function loadNewAlbums() {
         Authorization: auth_token,
       },
     });
-      let data = await res_pages.json();
-      const res = await fetch(query + `&page=${Math.floor(Math.random() * data.pagination.pages + 1)}`, {
+    let data = await res_pages.json();
+    const res = await fetch(
+      query + `&page=${Math.floor(Math.random() * data.pagination.pages + 1)}`,
+      {
         headers: {
           Authorization: auth_token,
         },
-      });
-      data = await res.json();
+      }
+    );
+    data = await res.json();
     console.log(data);
     data.results.forEach((album) => {
       loadNewSongs(album.id, album.thumb, album.uri);
@@ -111,14 +128,11 @@ async function loadNewSongs(album_id, cover, uri) {
   const songlist = document.getElementsByClassName("new-songs")[0];
   songlist.innerHTML = "";
   try {
-    const res = await fetch(
-      `https://api.discogs.com/releases/${album_id}`,
-      {
-        headers: {
-          Authorization: auth_token
-        },
-      }
-    );
+    const res = await fetch(`https://api.discogs.com/releases/${album_id}`, {
+      headers: {
+        Authorization: auth_token,
+      },
+    });
     let data = await res.json();
     console.log(data);
     data.tracklist.forEach((track) => {
@@ -142,7 +156,10 @@ async function loadNewSongs(album_id, cover, uri) {
         <a title="Search on Soundcloud" target="_blank" href="https://soundcloud.com/search?q=${title
           .replaceAll(/\([^)]*\)/g, "")
           .replace("&", "%26")}"><i class="fa-brands fa-soundcloud"></i></a>
-            <a title="Show on Discogs" target="_blank" href="https://discogs.com/${uri.replace(/^\//, "")}"><i class="fa-solid fa-record-vinyl"></i></a>
+            <a title="Show on Discogs" target="_blank" href="https://discogs.com/${uri.replace(
+              /^\//,
+              ""
+            )}"><i class="fa-solid fa-record-vinyl"></i></a>
             </div>
             `;
       songlist.append(newSong);
