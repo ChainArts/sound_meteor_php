7cb4532cf0c46b66258827355f74a29ce2a54135
Maximilian Roll <mroll.mmt-b2022@fh-salzburg.ac.at>
2023-05-30 22:43:42 +0200

    added edit fields

--- meteor.php
+++ meteor.php
@@ -1,4 +1,5 @@
 <?php
+$list_id = 1;
 include "functions.php";
 if (!isset($_SESSION["USER"])) {
     header('Location: schlingel.php');
@@ -20,8 +21,23 @@ if (isset($_GET['status'])) {
             <span>$msg</span>
           </div>";
 }
+
+try{
+    $sth = $dbh->prepare("SELECT name, creator_id, isshared FROM playlists WHERE playlist_id = ?");
+    $sth->execute(array($list_id));
+    $list = $sth->fetch();
+
+    $sth = $dbh->prepare("SELECT * FROM saved_in_playlist INNER JOIN tracks ON saved_in_playlist.track_id = tracks.track_id WHERE playlist_id = ?");
+    $sth->execute(array($list_id));
+    $tracks = $sth->fetchAll();
+}
+catch(Exception $e){
+    echo $e->getMessage();
+}
 ?>
-<h1><?= $pagetitle ?></h1>
+<div class="meteor-container">
+    <span class="meteor-title"><?=$list->name?> - Meteor</span>
+</div>
 
 
 
