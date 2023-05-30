7d2c25efcf3f9e65a919e34391a0e2084f00377a
Maximilian Roll <mroll.mmt-b2022@fh-salzburg.ac.at>
2023-05-31 01:22:48 +0200

    added more cases to display

--- meteor.php
+++ meteor.php
@@ -22,53 +22,109 @@ if (isset($_GET['status'])) {
           </div>";
 }
 
-try {
-    $sth = $dbh->prepare("SELECT * FROM playlists WHERE playlist_id = ?");
-    $sth->execute(array($list_id));
-    $list = $sth->fetch();
+if (isset($_GET['id'])) {
 
-    $sth = $dbh->prepare("SELECT username FROM users WHERE user_id = ?");
-    $sth->execute(array($list->creator_id));
-    $creator = $sth->fetch();
+    try {
+        $sth = $dbh->prepare("SELECT * FROM playlists WHERE playlist_id = ?");
+        $sth->execute(array($_GET['id']));
+        $list = $sth->fetch();
 
-    $sth = $dbh->prepare("SELECT * FROM saved_in_playlist INNER JOIN tracks ON saved_in_playlist.track_id = tracks.track_id WHERE playlist_id = ?");
-    $sth->execute(array($list_id));
-    $tracks = $sth->fetchAll();
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
-?>
-<div class="meteor-container">
-    <span class="meteor-title"><?= $list->name ?> - Meteor</span>
-    <div class="meteor-img"><img src="./media/SoundMeteor.svg" alt="Logo"></div>
-    <?php
-    foreach ($tracks as $track) {
-        $title = $track->band . " - " . $track->name;
-    ?>
-        <div class="new-song-wrapper">
-            <div class="song-cover">
-                <img src="" alt="Cover Image">
-            </div>
-            <span class="song-title"><?= $title ?></span>
-            <div class="song-links">
-                <a title="Search on Youtube" target="_blank" href="https://youtube.com"><i class="fa-brands fa-youtube"></i></a>
-                <a title="Search on SoundCloud" target="_blank" href="https://soundcloud.com"><i class="fa-brands fa-soundcloud"></i></a>
-                <a title="Show on Discogs" target="_blank" href="https://discogs.com"><i class="fa-solid fa-record-vinyl"></i></a>
-            </div>
-        </div>
-    <?php
+        $sth = $dbh->prepare("SELECT username FROM users WHERE user_id = ?");
+        $sth->execute(array($list->creator_id));
+        $creator = $sth->fetch();
+
+        $sth = $dbh->prepare("SELECT * FROM saved_in_playlist INNER JOIN tracks ON saved_in_playlist.track_id = tracks.track_id WHERE playlist_id = ?");
+        $sth->execute(array($_GET['id']));
+        $tracks = $sth->fetchAll();
+    } catch (Exception $e) {
+        echo $e->getMessage();
     }
-    ?>
-    <div class="meteor-details">
-        <span><?= $creator->username ?></span>
-        <span>Views: <?=$list->view_amount?></span>
-        <span>Shared: <?php if($list->isshared) echo "true"; else echo "false"; ?></span>
+?>
+    <div class="meteor-container">
+        <span class="meteor-title"><?= $list->name ?> - Meteor</span>
+        <div class="meteor-img"><img src="./media/SoundMeteor.svg" alt="Logo"></div>
+        <?php
+        foreach ($tracks as $track) {
+            $title = $track->band . " - " . $track->name;
+        ?>
+            <div class="new-song-wrapper">
+                <div class="song-cover">
+                    <img src="" alt="Cover Image">
+                </div>
+                <span class="song-title"><?= $title ?></span>
+                <div class="song-links">
+                    <a title="Search on Youtube" target="_blank" href="https://youtube.com"><i class="fa-brands fa-youtube"></i></a>
+                    <a title="Search on SoundCloud" target="_blank" href="https://soundcloud.com"><i class="fa-brands fa-soundcloud"></i></a>
+                    <a title="Show on Discogs" target="_blank" href="https://discogs.com"><i class="fa-solid fa-record-vinyl"></i></a>
+                </div>
+            </div>
+        <?php
+        }
+        ?>
+        <div class="meteor-details">
+            <span><?= $creator->username ?></span>
+            <span>Views: <?= $list->view_amount ?></span>
+            <span>Shared: <?php if ($list->isshared) echo "true";
+                            else echo "false"; ?></span>
+        </div>
     </div>
-</div>
+    <?php
+} else {
+    try {
+        $sth = $dbh->prepare("SELECT * FROM playlists WHERE creator_id = ?");
+        $sth->execute(array($_SESSION['USER_ID']));
+        $list = $sth->fetch();
 
+        if ($list) {
+            $sth = $dbh->prepare("SELECT username FROM users WHERE user_id = ?");
+            $sth->execute(array($list->creator_id));
+            $creator = $sth->fetch();
+            
+            $sth = $dbh->prepare("SELECT * FROM saved_in_playlist INNER JOIN tracks ON saved_in_playlist.track_id = tracks.track_id WHERE playlist_id = ?");
+            $sth->execute(array($list->playlist_id));
+            $tracks = $sth->fetchAll();
+    ?>
+            <div class="meteor-container">
+                <span class="meteor-title"><?= $list->name ?> - Meteor</span>
+                <div class="meteor-img"><img src="./media/SoundMeteor.svg" alt="Logo"></div>
+                <?php
+                foreach ($tracks as $track) {
+                    $title = $track->band . " - " . $track->name;
+                ?>
+                    <div class="new-song-wrapper">
+                        <div class="song-cover">
+                            <img src="" alt="Cover Image">
+                        </div>
+                        <span class="song-title"><?= $title ?></span>
+                        <div class="song-links">
+                            <a title="Search on Youtube" target="_blank" href="https://youtube.com"><i class="fa-brands fa-youtube"></i></a>
+                            <a title="Search on SoundCloud" target="_blank" href="https://soundcloud.com"><i class="fa-brands fa-soundcloud"></i></a>
+                            <a title="Show on Discogs" target="_blank" href="https://discogs.com"><i class="fa-solid fa-record-vinyl"></i></a>
+                        </div>
+                    </div>
+                <?php
+                }
+                ?>
+                <div class="meteor-details">
+                    <span><?= $creator->username ?></span>
+                    <span>Views: <?= $list->view_amount ?></span>
+                    <span>Shared: <?php if ($list->isshared) echo "true";
+                                    else echo "false"; ?></span>
+                </div>
+            </div>
 
-
+        <?php
+        } else {
+        ?>
+            <div class="meteor-container">
+                <span class="meteor-title">No Meteors Found!</span>
+            </div>
 
 <?php
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
 include "footer.php";
 ?>
