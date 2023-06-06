35d37a36872ba7e6b0e54700288cc57c3f45a439
Maximilian Roll <mroll.mmt-b2022@fh-salzburg.ac.at>
2023-06-06 03:48:14 +0200

    added about section

--- about.php
+++ about.php
@@ -7,6 +7,40 @@ include "header.php";
 
 <h1><?= $pagetitle ?></h1>
 
+<section class="about">
+<h2>Imprint</h2>
+<p>
+Multi Media Project 1 - This Website was created as part of the bachelor's degree "MultiMediaTechnology" at the <a class="navLink" href="https://www.fh-salzburg.ac.at/">University of Applied Science in Salzburg</a> in the second semester.
+<h2>Contact</h2>
+<p>Maximilian Roll</p>
+<p>fhs49272@fh-salzburg.ac.at
+<h2>Used Libraries/Assets</h2>
+<div>
+    <ul>
+        <li><a class="navLink" href="https://www.discogs.com/">Discogs: Song-API</a></li>
+        <li><a class="navLink" href="https://fontawesome.com/">Font-Awesome: Icons</a></li>
+        <li><a class="navLink" href="https://grsmto.github.io/simplebar/">Simplebar.js: Scrollbar Library</a></li>
+        <li><a class="navLink" href="https://vincentgarreau.com/particles.js/"> Particles.js: Background Effects</a></li>
+        <li>Images/Illustrations: Maximilian Roll | Sandra Scheipl</li>
+    </ul>
+</div>
+
+<h2>Privacy Policy</h2>
+<p>
+We here at Sound Meteor value and respect your privacy and therefore we only store essential cookies to provide you with access to your personalized account.<br><br>
+
+You will not get any spam messages or newsletters when you create an account.<br><br>
+
+We do not sell, trade, or rent your personal information to third parties.<br><br>
+
+We take reasonable measures to protect your personal information from unauthorized access.<br><br>
+
+Our website may contain links to third-party websites, and we are not responsible for their privacy practices.
+</p>
+
+</section>
+
+
 <?php
 include "footer.php";
 ?>
